6428ac7f9f2863b394e6257c43f79bdf35517d2a
Haribaskar619 <79138852+Haribaskar619@users.noreply.github.com>
2022-01-03 18:53:39 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,48 @@
+// Zen Portal Day-1 Task
+
+// 1. Gone through the three Links.
+//     Scripting Language vs Programming Language
+//     Statically Typed vs Dynamically Typed languages
+//     Programming pradigms
+
+// 2. Write a blog on Difference between HTTP1.1 vs HTTP2
+
+//     HTTP is a protocol Language which processes the user request to access certain information or data. The first and foremost difference factor between HTTP 1.1 
+//     & HTTP2 is Speed of Information Loading or the time taken to show the information that the user has requested.
+    
+//     HTTP 1.1 was introduced in 1997 whereas HTTP2 was introduced in 2015. Almost 18 years of research work went in during the version upgrade.
+    
+//     In HTTP (1.1) 6 requests combined for one three way handshake.Whereas in HTTP(2) there is a technology called multiplexing.
+//     Some of the optimizations used in the Http 1.1 version are sprinting, inlining, domain sharding, and concatenating.but HTTP2 protocol version removes the need for unnecessary optimization hacks.
+//     Http 1.1 provides faster delivery of web pages and reduces web traffic if you compare it to Http 1.0. However, there is an increased risk of network congestion. 
+//     The Http 2 version utilizes multiplexing and server pushes to effectively reduce the page load time by a greater margin along with being sensitive to network delays.
+       
+// 3. Write a blog about objects and its internal representation in Javascript
+
+//     Objects are the basic building blocks of modern day Javascript. 
+//     Unlike the primitive data types objects hold combined data types together in a form of key:value pairs.
+    
+//     This object has two things
+//      Object Properties
+//     Object Methods
+    
+//     Object Properties:
+    
+//     The name:values pairs in JavaScript objects are called properties.
+    
+//     Example :   
+//     var car = {
+//     Make: “Mercedes”, 
+//     Model: “C-Class”,
+//      Color: “White”,
+//      Fuel: Diesel,
+//      Weight: “850kg”,
+//     Mileage: “8Kmpl”,
+//      Rating: 4.5
+//     };    
+//     The object properties can be different primitive values, other objects and functions.Properties can usually be changed, added, and deleted, but some are read only. So properties are the values associated with Javascript Object.
+    
+//     Object Method:
+//     An object method is an object property containing a function definition.Actions that performed on objects is called as object methods.
+//     From the above example if we want to change the color then we can write a function to change it. Like wise some actions can be performed in the object is called Object methods.
+    
